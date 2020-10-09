30396966260d7ea8f1d1b55f28cd4630d36293af
julijaivankovic <julija.ivankovic1@gmail.com>
2020-10-09 14:11:31 +0200

    updating resources

--- CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -4,25 +4,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace CourseLibrary.API.Models
 {
-    [CourseTitleMustBeDifferentFromDescription(
-        ErrorMessage = "Title must be different from description.")]
-    public class CourseForCreationDto //: IValidatableObject
+    public class CourseForCreationDto : CourseForManipulationDto
     {
-        [Required(ErrorMessage = "You should fill out a title.")]
-        [MaxLength(100, ErrorMessage = "The title shouldn't have more than 100 characters.")]
-        public string Title { get; set; }
-
-        [MaxLength(500, ErrorMessage = "The title shouldn't have more than 500 characters.")]
-        public string Description { get; set; }
-
-        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        //{
-        //    if (Title == Description)
-        //    {
-        //        yield return new ValidationResult(
-        //            "The provided description should be different from the title.",
-        //            new[] { "CourseForCreationDto" });
-        //    }
-        //}
     }
 }
